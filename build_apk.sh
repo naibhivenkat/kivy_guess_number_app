36093dc8971954512c5810c676f149b2306da22f
naibhivenkat <venkat.naibhi@gmail.com>
2025-06-30 23:02:46 +0530

    apk build fix

--- build_apk.sh
+++ build_apk.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 set -e
 
+echo "== Installing dependencies =="
 apt-get update
 apt-get install -y zip unzip openjdk-17-jdk wget git \
   python3-dev clang cmake libffi-dev libssl-dev libgl1-mesa-dev \
   autoconf automake libtool m4 texinfo
 
 pip install --upgrade pip
-pip install Cython==0.29.36 python-for-android==2024.1.21
+pip install Cython==0.29.36
+pip install git+https://github.com/kivy/python-for-android@develop
 
 export ANDROID_SDK_ROOT=/opt/android-sdk
 export ANDROID_NDK_HOME=$ANDROID_SDK_ROOT/ndk/25.2.9519653
 
 mkdir -p $ANDROID_SDK_ROOT/cmdline-tools
 wget -q https://dl.google.com/android/repository/commandlinetools-linux-9477386_latest.zip -O sdk-tools.zip
-unzip sdk-tools.zip -d $ANDROID_SDK_ROOT/cmdline-tools
+unzip -q sdk-tools.zip -d $ANDROID_SDK_ROOT/cmdline-tools
 mv $ANDROID_SDK_ROOT/cmdline-tools/cmdline-tools $ANDROID_SDK_ROOT/cmdline-tools/latest
 
 yes | $ANDROID_SDK_ROOT/cmdline-tools/latest/bin/sdkmanager --licenses
@@ -24,7 +26,8 @@ $ANDROID_SDK_ROOT/cmdline-tools/latest/bin/sdkmanager \
   'build-tools;36.0.0' \
   'ndk;25.2.9519653'
 
-python3 -m pythonforandroid.toolchain apk \
+echo "== Building APK =="
+python3 python3 -m pythonforandroid.toolchain apk \
   --private . \
   --package=com.gtn.app \
   --name="Guess the Number" \
@@ -32,7 +35,9 @@ python3 -m pythonforandroid.toolchain apk \
   --bootstrap=sdl2 \
   --requirements=python3,kivy \
   --arch=arm64-v8a \
+  --dist_name=guessnumber_dist \
   --android_api=34 \
   --sdk_dir=$ANDROID_SDK_ROOT \
   --ndk_dir=$ANDROID_NDK_HOME \
-  --output=Guess_the_Number.apk
+  --output "Guess_the_Number.apk"
+
